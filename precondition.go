2b3c072e72e3cd1aaa12bd8908a5c19243483a52
Alexander Trakhimenok <alex@trakhimenok.com>
2021-10-06 23:54:33 +0100

    doc strings

--- precondition.go
+++ precondition.go
@@ -2,8 +2,10 @@ package dalgo
 
 import "time"
 
+// Precondition defines precondition
 type Precondition = func(preconditions *preConditions)
 
+// Preconditions defines preconditions
 type Preconditions interface {
 	Exists() bool
 }
@@ -13,22 +15,26 @@ type preConditions struct {
 	lastUpdateTime time.Time
 }
 
+// Exists indicate exists precondition
 func (v preConditions) Exists() bool {
 	return v.exists
 }
 
+// WithExistsPrecondition sets exists precondition
 func WithExistsPrecondition() func(preconditions *preConditions) {
 	return func(preconditions *preConditions) {
 		preconditions.exists = true
 	}
 }
 
+// WithLastUpdateTimePrecondition sets last update time
 func WithLastUpdateTimePrecondition(t time.Time) func(preconditions *preConditions) {
 	return func(preconditions *preConditions) {
 		preconditions.lastUpdateTime = t
 	}
 }
 
+// GetPreconditions create Preconditions
 func GetPreconditions(items ...Precondition) Preconditions {
 	var result preConditions
 	for _, precondition := range items {
