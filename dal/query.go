9f1652389d9d39830b12b99ef5b3bd5382d71710
Alexander Trakhimenok <alex@trakhimenok.com>
2022-05-04 00:39:15 +0100

    fixed math.MaxInt => math.MaxInt64 to support go v1.16

--- dal/query.go
+++ dal/query.go
@@ -138,7 +138,7 @@ func (v groupCondition) String() string {
 func ReadAll(_ context.Context, reader Reader, limit int) (records []Record, err error) {
 	var record Record
 	if limit <= 0 {
-		limit = math.MaxInt
+		limit = math.MaxInt64
 	}
 	for i := 0; i < limit; i++ {
 		if i >= limit {
