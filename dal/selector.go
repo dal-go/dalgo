4fb8da5e3cb78b10687cd6cebadab5bace8152e8
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-04-19 16:09:29 +0100

    feat: simplify interface

--- dal/selector.go
+++ dal/selector.go
@@ -5,54 +5,69 @@ import (
 	"errors"
 )
 
-// QueryExecutor links Query definition to a specific session (or transaction).
+// QueryExecutor is a query executor that returns a reader + related helper methods.
 type QueryExecutor interface {
 
-	// ExecuteQuery creates a Selector for the given query.
-	ExecuteQuery(c context.Context, query Query, f func(c context.Context, selector Selector) error) (err error)
-}
-
-// Selector is a query executor that returns a reader + helper methods.
-type Selector interface {
-
-	// SelectReader returns a reader for the given query to read records 1 by 1 sequentially.
+	// QueryReader returns a reader for the given query to read records 1 by 1 sequentially.
 	// The Reader.Next() method returns ErrNoMoreRecords when there are no more records.
-	SelectReader(c context.Context) (Reader, error)
+	QueryReader(c context.Context, query Query) (Reader, error)
 
-	// SelectAllRecords is a helper method that returns all records for the given query.
-	// It reads reader created by SelectReader until it returns ErrNoMoreRecords.
-	SelectAllRecords(ctx context.Context) ([]Record, error)
-
-	// SelectAllIDs returns all IDs for the given query without reading the full record.
-	SelectAllIDs(c context.Context) (ids []any, err error)
-
-	// SelectAllStrIDs is a helper method that returns all IDs as strings for the given query.
-	SelectAllStrIDs(ctx context.Context) ([]string, error)
-
-	// SelectAllIntIDs is a helper method that returns all IDs as integers for the given query.
-	SelectAllIntIDs(ctx context.Context) ([]int, error)
-
-	// SelectAllInt64IDs is a helper method that returns all IDs as int64 for the given query.
-	SelectAllInt64IDs(ctx context.Context) ([]int64, error)
+	// QueryAllRecords is a helper method that returns all records for the given query.
+	// It reads reader created by QueryReader until it returns ErrNoMoreRecords.
+	// If you are interested only in IDs, use like:
+	//
+	//		reader, err := selector.SelectReader(ctx)
+	//      // handle err
+	//		var ids []int
+	//		ids, err = dal.SelectAllIDs[int](reader)
+	QueryAllRecords(ctx context.Context, query Query) (records []Record, err error)
 }
 
-var _ Selector = (*selector)(nil)
+var _ QueryExecutor = (*selector)(nil)
 
 type selector struct {
 	query     Query
 	getReader func(c context.Context, query Query) (Reader, error)
 }
 
-func (s selector) SelectReader(c context.Context) (Reader, error) {
-	return s.getReader(c, s.query)
+func (s selector) QueryReader(c context.Context, query Query) (Reader, error) {
+	return s.getReader(c, query)
 }
 
-func (s selector) SelectAllRecords(c context.Context) (records []Record, err error) {
+// QueryAllRecords is a helper method that for a given reader returns all records as a slice.
+func (s selector) QueryAllRecords(c context.Context, query Query) (records []Record, err error) {
 	var reader Reader
-	if reader, err = s.getReader(c, s.query); err != nil {
+	if reader, err = s.getReader(c, query); err != nil {
 		return
 	}
-	for i := 0; s.query.Limit <= 0 || i <= s.query.Limit; i++ {
+	return SelectAllRecords(reader, query.Limit)
+}
+
+type ReaderProvider = func(c context.Context, query Query) (reader Reader, err error)
+
+func NewQueryExecutor(getReader ReaderProvider) QueryExecutor {
+	if getReader == nil {
+		panic("getReader is a required parameter, got nil")
+	}
+	return &selector{getReader: getReader}
+}
+
+func SelectAllIDs[T comparable](reader Reader, limit int) (ids []T, err error) {
+	for i := 0; limit <= 0 || i < limit; i++ {
+		var record Record
+		if record, err = reader.Next(); err != nil {
+			if errors.Is(err, ErrNoMoreRecords) {
+				err = nil
+			}
+			return
+		}
+		ids = append(ids, record.Key().ID.(T))
+	}
+	return
+}
+
+func SelectAllRecords(reader Reader, limit int) (records []Record, err error) {
+	for i := 0; limit <= 0 || i < limit; i++ {
 		var record Record
 		if record, err = reader.Next(); err != nil {
 			if errors.Is(err, ErrNoMoreRecords) {
@@ -63,59 +78,4 @@ func (s selector) SelectAllRecords(c context.Context) (records []Record, err err
 		records = append(records, record)
 	}
 	return
-}
-
-func (s selector) SelectAllIDs(c context.Context) (ids []any, err error) {
-	return ids, s.selectAllIDsWorker(c, s.query, func(id any) error {
-		ids = append(ids, id)
-		return nil
-	})
-}
-
-func (s selector) SelectAllStrIDs(c context.Context) (ids []string, err error) {
-	return ids, s.selectAllIDsWorker(c, s.query, func(id any) error {
-		ids = append(ids, id.(string))
-		return nil
-	})
-}
-
-func (s selector) SelectAllIntIDs(c context.Context) (ids []int, err error) {
-	return ids, s.selectAllIDsWorker(c, s.query, func(id any) error {
-		ids = append(ids, id.(int))
-		return nil
-	})
-}
-
-func (s selector) SelectAllInt64IDs(c context.Context) (ids []int64, err error) {
-	return ids, s.selectAllIDsWorker(c, s.query, func(id any) error {
-		ids = append(ids, id.(int64))
-		return nil
-	})
-}
-
-func NewSelector(query Query, getReader func(c context.Context, query Query) (Reader, error)) Selector {
-	if getReader == nil {
-		panic("getReader is a required parameter, got nil")
-	}
-	return &selector{query: query, getReader: getReader}
-}
-
-func (s selector) selectAllIDsWorker(c context.Context, query Query, addID func(id any) error) (err error) {
-	var reader Reader
-	if reader, err = s.getReader(c, query); err != nil {
-		return err
-	}
-	for i := 0; query.Limit <= 0 || i <= query.Limit; i++ {
-		var record Record
-		if record, err = reader.Next(); err != nil {
-			if errors.Is(err, ErrNoMoreRecords) {
-				err = nil
-			}
-			return err
-		}
-		if err = addID(record.Key().ID); err != nil {
-			return err
-		}
-	}
-	return err
 }
--- dal/selector_test.go
+++ dal/selector_test.go
@@ -10,14 +10,14 @@ import (
 func TestNewSelector(t *testing.T) {
 	t.Run("panic_on_nil", func(t *testing.T) {
 		assert.Panics(t, func() {
-			NewSelector(Query{}, nil)
+			NewQueryExecutor(nil)
 		})
 	})
 	t.Run("should_pass", func(t *testing.T) {
-		getReader := func(c context.Context, query Query) (Reader, error) {
+		var getReader = func(c context.Context, query Query) (Reader, error) {
 			return nil, nil
 		}
-		selector := NewSelector(Query{}, getReader)
+		selector := NewQueryExecutor(getReader)
 		assert.NotNil(t, selector)
 	})
 }
@@ -46,7 +46,7 @@ func Test_selector_SelectReader(t *testing.T) {
 				query:     tt.args.query,
 				getReader: tt.fields.getReader,
 			}
-			got, err := s.SelectReader(tt.args.c)
+			got, err := s.QueryReader(tt.args.c, tt.args.query)
 			if !tt.wantErr(t, err, fmt.Sprintf("Select(%v, %v)", tt.args.c, tt.args.query)) {
 				return
 			}
@@ -55,14 +55,14 @@ func Test_selector_SelectReader(t *testing.T) {
 	}
 }
 
-func Test_selector_SelectAll(t *testing.T) {
+func Test_selector_QueryAllRecords(t *testing.T) {
 	t.Skip("TODO: implement test")
 	type fields struct {
-		query     Query
 		getReader func(c context.Context, query Query) (Reader, error)
 	}
 	type args struct {
-		c context.Context
+		c     context.Context
+		query Query
 	}
 	tests := []struct {
 		name        string
@@ -76,174 +76,13 @@ func Test_selector_SelectAll(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := selector{
-				query:     tt.fields.query,
 				getReader: tt.fields.getReader,
 			}
-			gotRecords, err := s.SelectAllRecords(tt.args.c)
+			gotRecords, err := s.QueryAllRecords(tt.args.c, tt.args.query)
 			if !tt.wantErr(t, err, fmt.Sprintf("SelectAll(%v)", tt.args.c)) {
 				return
 			}
 			assert.Equalf(t, tt.wantRecords, gotRecords, "SelectAll(%v)", tt.args.c)
-		})
-	}
-}
-
-func Test_selector_SelectAllIDs(t *testing.T) {
-	t.Skip("TODO: implement test")
-	type fields struct {
-		query     Query
-		getReader func(c context.Context, query Query) (Reader, error)
-	}
-	type args struct {
-		c context.Context
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantIds []any
-		wantErr assert.ErrorAssertionFunc
-	}{
-		{name: "empty", args: args{c: context.Background()}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := selector{
-				query:     tt.fields.query,
-				getReader: tt.fields.getReader,
-			}
-			gotIds, err := s.SelectAllIDs(tt.args.c)
-			if !tt.wantErr(t, err, fmt.Sprintf("SelectAllIDs(%v)", tt.args.c)) {
-				return
-			}
-			assert.Equalf(t, tt.wantIds, gotIds, "SelectAllIDs(%v)", tt.args.c)
-		})
-	}
-}
-
-func Test_selector_SelectAllInt64IDs(t *testing.T) {
-	t.Skip("TODO: implement test")
-	type fields struct {
-		query     Query
-		getReader func(c context.Context, query Query) (Reader, error)
-	}
-	type args struct {
-		c context.Context
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantIds []int64
-		wantErr assert.ErrorAssertionFunc
-	}{
-		{name: "empty", args: args{c: context.Background()}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := selector{
-				query:     tt.fields.query,
-				getReader: tt.fields.getReader,
-			}
-			gotIds, err := s.SelectAllInt64IDs(tt.args.c)
-			if !tt.wantErr(t, err, fmt.Sprintf("SelectAllInt64IDs(%v)", tt.args.c)) {
-				return
-			}
-			assert.Equalf(t, tt.wantIds, gotIds, "SelectAllInt64IDs(%v)", tt.args.c)
-		})
-	}
-}
-
-func Test_selector_SelectAllIntIDs(t *testing.T) {
-	t.Skip("TODO: implement test")
-	type fields struct {
-		query     Query
-		getReader func(c context.Context, query Query) (Reader, error)
-	}
-	type args struct {
-		c context.Context
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantIds []int
-		wantErr assert.ErrorAssertionFunc
-	}{
-		{name: "empty", args: args{c: context.Background()}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := selector{
-				query:     tt.fields.query,
-				getReader: tt.fields.getReader,
-			}
-			gotIds, err := s.SelectAllIntIDs(tt.args.c)
-			if !tt.wantErr(t, err, fmt.Sprintf("SelectAllIntIDs(%v)", tt.args.c)) {
-				return
-			}
-			assert.Equalf(t, tt.wantIds, gotIds, "SelectAllIntIDs(%v)", tt.args.c)
-		})
-	}
-}
-
-func Test_selector_SelectAllStrIDs(t *testing.T) {
-	t.Skip("TODO: implement test")
-	type fields struct {
-		query     Query
-		getReader func(c context.Context, query Query) (Reader, error)
-	}
-	type args struct {
-		c context.Context
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantIds []string
-		wantErr assert.ErrorAssertionFunc
-	}{
-		{name: "empty"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := selector{
-				query:     tt.fields.query,
-				getReader: tt.fields.getReader,
-			}
-			gotIds, err := s.SelectAllStrIDs(tt.args.c)
-			if !tt.wantErr(t, err, fmt.Sprintf("SelectAllStrIDs(%v)", tt.args.c)) {
-				return
-			}
-			assert.Equalf(t, tt.wantIds, gotIds, "SelectAllStrIDs(%v)", tt.args.c)
-		})
-	}
-}
-
-func Test_selector_selectAllIDsWorker(t *testing.T) {
-	t.Skip("TODO: implement test")
-	type fields struct {
-		getReader func(c context.Context, query Query) (Reader, error)
-	}
-	type args struct {
-		c     context.Context
-		query Query
-		addID func(id any) error
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr assert.ErrorAssertionFunc
-	}{
-		{name: "empty"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := selector{
-				getReader: tt.fields.getReader,
-			}
-			tt.wantErr(t, s.selectAllIDsWorker(tt.args.c, tt.args.query, tt.args.addID), fmt.Sprintf("selectAllIDsWorker(%v, %v)", tt.args.c, tt.args.query))
 		})
 	}
 }
