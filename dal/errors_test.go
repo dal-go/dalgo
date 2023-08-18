97066e147c1f32257d620de09dd2be83d9c1396e
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-08-18 09:01:35 +0100

    chore(test): errors_test.go

--- dal/errors_test.go
+++ dal/errors_test.go
@@ -86,3 +86,115 @@ func TestErrDuplicateUser_Error(t *testing.T) {
 		}
 	}
 }
+
+func TestIsNotFound(t *testing.T) {
+	type test struct {
+		name string
+		err  error
+		want bool
+	}
+	tests := []test{
+		{
+			name: "nil",
+			err:  nil,
+			want: false,
+		},
+		{
+			name: "nil",
+			err:  errors.New("some error"),
+			want: false,
+		},
+		{
+			name: "ErrRecordNotFound",
+			err:  ErrRecordNotFound,
+			want: true,
+		},
+		{
+			name: "errNotFoundByKey",
+			err:  errNotFoundByKey{cause: ErrRecordNotFound},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, IsNotFound(tt.err))
+		})
+	}
+}
+
+func TestErrNotFoundByKey_Key(t *testing.T) {
+	err := errNotFoundByKey{
+		key: &Key{ID: "id1", collection: "records"},
+	}
+	key := err.Key()
+	assert.NotNil(t, key)
+	assert.Equal(t, "id1", key.ID)
+	assert.Equal(t, "records", key.collection)
+}
+
+func TestErrNotFoundByKey_Cause(t *testing.T) {
+	cause := errors.New("some cause")
+	err := errNotFoundByKey{
+		cause: cause,
+	}
+	assert.Equal(t, cause, err.Cause())
+}
+
+func TestErrNotFoundByKey_Error(t *testing.T) {
+	type test struct {
+		name string
+		err  errNotFoundByKey
+	}
+	tests := []test{
+		{
+			name: "no_cause",
+			err:  errNotFoundByKey{key: &Key{ID: "id1", collection: "records"}},
+		},
+		{
+			name: "cause_is_ErrRecordNotFound",
+			err: errNotFoundByKey{
+				key:   &Key{ID: "id1", collection: "records"},
+				cause: ErrRecordNotFound,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := tt.err.Error()
+			assert.True(t, strings.Contains(s, tt.err.key.ID.(string)))
+			assert.True(t, strings.Contains(s, tt.err.key.collection))
+			if tt.err.cause == nil {
+				assert.True(t, strings.Contains(s, "record not found"))
+			} else {
+				assert.True(t, strings.Contains(s, tt.err.cause.Error()))
+			}
+		})
+	}
+}
+
+func TestRollbackError_Error(t *testing.T) {
+	type test struct {
+		name string
+		err  rollbackError
+	}
+	tests := []test{
+		{
+			name: "empty",
+			err:  rollbackError{},
+		},
+		{
+			name: "with_original_error",
+			err:  rollbackError{originalErr: errors.New("some error")},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := tt.err.Error()
+			assert.True(t, strings.Contains(s, "rollback failed"), "expected 'rollback error' in %v", s)
+			if tt.err.originalErr != nil {
+				assert.True(t, strings.Contains(s, "original error"))
+				assert.True(t, strings.Contains(s, tt.err.originalErr.Error()))
+			}
+		})
+	}
+}
