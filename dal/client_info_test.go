2d9697caac4f5ea51070cbb7c5b89648295d4844
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-08-18 08:40:55 +0100

    chore(test): ClientInfo

--- dal/client_info_test.go
+++ dal/client_info_test.go
@@ -24,3 +24,67 @@ func TestNewClientInfo(t *testing.T) {
 		})
 	}
 }
+
+func TestClientInfo_Fields(t *testing.T) {
+	type fields struct {
+		driver  string
+		version string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   fields
+	}{
+		{"should_pass", fields{"sql", "v1"}, fields{driver: "sql", version: "v1"}},
+		{"should_pass", fields{"firestore", "v2"}, fields{driver: "firestore", version: "v2"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := clientInfo{
+				driver:  tt.fields.driver,
+				version: tt.fields.version,
+			}
+			if got := v.Driver(); got != tt.want.driver {
+				t.Errorf("Driver() = %v, want %v", got, tt.want)
+			}
+			if got := v.Version(); got != tt.want.version {
+				t.Errorf("Version() = %v, want %v", got, tt.want)
+			}
+			if s := v.String(); s != tt.want.driver+"@"+tt.want.version {
+				t.Errorf("String() = %v, want %v", s, tt.want.driver+"@"+tt.want.version)
+			}
+		})
+	}
+}
+
+func TestClientInfo_Equals(t *testing.T) {
+	type fields struct {
+		driver  string
+		version string
+	}
+	type args struct {
+		other ClientInfo
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{"should_pass", fields{"sql", "v1"}, args{clientInfo{driver: "sql", version: "v1"}}, true},
+		{"should_pass", fields{"firestore", "v2"}, args{clientInfo{driver: "firestore", version: "v2"}}, true},
+		{"should_pass", fields{"firestore", "v2"}, args{clientInfo{driver: "firestore", version: "v1"}}, false},
+		{"should_pass", fields{"firestore", "v2"}, args{clientInfo{driver: "sql", version: "v2"}}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := clientInfo{
+				driver:  tt.fields.driver,
+				version: tt.fields.version,
+			}
+			if got := v.Equals(tt.args.other); got != tt.want {
+				t.Errorf("Equals() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
