49c24f29cde684d9d36370f60fcbe95ecb2f9e6d
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-08-19 08:06:47 +0100

    chore(test): added TestGroupCondition

---
+++ dal/q_group_condition_test.go
@@ -0,0 +1,35 @@
+package dal
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGroupCondition(t *testing.T) {
+	for _, tt := range []struct {
+		name           string
+		gc             GroupCondition
+		expectedString string
+	}{
+		{name: "empty", gc: GroupCondition{}, expectedString: "()"},
+		{name: "2items", gc: GroupCondition{
+			operator: Equal,
+			conditions: []Condition{
+				String("a"),
+				String("b"),
+			},
+		}, expectedString: "('a'=='b')"},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Run("String", func(t *testing.T) {
+				assert.Equal(t, tt.expectedString, tt.gc.String())
+			})
+			t.Run("Conditions", func(t *testing.T) {
+				assert.Equal(t, tt.gc.conditions, tt.gc.Conditions())
+			})
+			t.Run("Operator", func(t *testing.T) {
+				assert.Equal(t, tt.gc.operator, tt.gc.Operator())
+			})
+		})
+	}
+}
