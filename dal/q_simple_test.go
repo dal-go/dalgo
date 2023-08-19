3c72bbb40cc2ddf312ba7a9f1ee3456aa27f9d89
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-08-19 14:49:56 +0100

    chore(lint): TestSimpleQuery

--- dal/q_simple_test.go
+++ dal/q_simple_test.go
@@ -2,6 +2,7 @@ package dal
 
 import (
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
 )
 
@@ -9,14 +10,24 @@ func TestSimpleQuery(t *testing.T) {
 	newRecord := func() Record {
 		return nil
 	}
-	var q = From("test",
+	var qb = From("test",
 		Field("field_1").EqualTo("value_1"),
-	).SelectInto(newRecord)
+	).
+		Where().
+		WhereField("field_2", Equal, "value_2").
+		Limit(10).
+		Offset(20).
+		StartFrom(Cursor("cursor_1")).
+		OrderBy()
+
+	q := qb.SelectInto(newRecord)
 	assert.NotNil(t, q)
 	assert.NotNil(t, q.From())
 	assert.Equal(t, "test", q.From().Name)
 	assert.NotNil(t, q.Where())
 	assert.NotNil(t, q.Into())
 	//assert.Equal(t, newRecordWithOnlyKey, q.Into)
-	t.Log("\n" + q.String())
+
+	q = qb.SelectKeysOnly(reflect.String)
+	assert.Equal(t, reflect.String, q.IDKind())
 }
