fee435fdf4c4c0869e5b1a2b807b55517cbe350c
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-08-19 07:46:15 +0100

    chore(test): added TestEmptyReader

--- dal/reader_test.go
+++ dal/reader_test.go
@@ -89,3 +89,23 @@ func TestRecordsReader(t *testing.T) {
 		}
 	})
 }
+
+func TestEmptyReader(t *testing.T) {
+	t.Run("Close", func(t *testing.T) {
+		reader := &EmptyReader{}
+		err := reader.Close()
+		assert.Nil(t, err)
+	})
+	t.Run("Cursor", func(t *testing.T) {
+		reader := &EmptyReader{}
+		cursor, err := reader.Cursor()
+		assert.Equal(t, ErrNotSupported, err)
+		assert.Equal(t, "", cursor)
+	})
+	t.Run("Next", func(t *testing.T) {
+		reader := &EmptyReader{}
+		record, err := reader.Next()
+		assert.Equal(t, ErrNoMoreRecords, err)
+		assert.Nil(t, record)
+	})
+}
