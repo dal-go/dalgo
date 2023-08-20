d3d129847ef9805b6e889a5f40cacc7bb4bdc5db
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-08-20 08:20:28 +0100

    chore(test): TestSelectAll

--- dal/query_select_funcs_test.go
+++ dal/query_select_funcs_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestSelectAllIDs(t *testing.T) {
+func TestSelectAll(t *testing.T) {
 	type args struct {
 		reader Reader
 		limit  int
@@ -22,17 +22,29 @@ func TestSelectAllIDs(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotIds, err := SelectAllIDs[int](tt.args.reader, tt.args.limit)
-			if tt.wantErr == nil && err != nil {
-				t.Errorf("unexpected error: %v", err)
+			assertErr := func(err error) {
+				if tt.wantErr == nil && err != nil {
+					t.Errorf("unexpected error: %v", err)
+				}
+				if tt.wantErr != nil && err == nil {
+					t.Errorf("expected error: %v", tt.wantErr)
+				}
+				if tt.wantErr != nil && err != nil && !errors.Is(err, tt.wantErr) {
+					t.Errorf("expected %v, got %v", tt.wantErr, err)
+				}
 			}
-			if tt.wantErr != nil && err == nil {
-				t.Errorf("expected error: %v", tt.wantErr)
-			}
-			if tt.wantErr != nil && err != nil && !errors.Is(err, tt.wantErr) {
-				t.Errorf("expected %v, got %v", tt.wantErr, err)
-			}
-			assert.Equal(t, tt.wantIds, gotIds)
+			t.Run("SelectAllIDs", func(t *testing.T) {
+				gotIds, err := SelectAllIDs[int](tt.args.reader, tt.args.limit)
+				assertErr(err)
+				assert.Equal(t, tt.wantIds, gotIds)
+			})
+			t.Run("SelectAllRecords", func(t *testing.T) {
+				gotRecords, err := SelectAllRecords(tt.args.reader, tt.args.limit)
+				assertErr(err)
+				if err == nil {
+					assert.NotNil(t, gotRecords)
+				}
+			})
 		})
 	}
 }
