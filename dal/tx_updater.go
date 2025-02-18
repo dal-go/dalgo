b2e7da4893073257e35947722f77452aed81a7c6
Alexander Trakhimenok <alex@trakhimenok.com>
2025-02-18 17:35:18 +0000

    feat: UpdateRecord(ctx context.Context, record Record, updates []Update, preconditions ...Precondition) error

--- dal/tx_updater.go
+++ dal/tx_updater.go
@@ -5,8 +5,14 @@ import "context"
 // Updater defines a function to update a single record in database by key
 type Updater interface {
 
-	// Update updates a single record in database by key
+	// Update updates a single record in a database by key
 	Update(ctx context.Context, key *Key, updates []Update, preconditions ...Precondition) error
+
+	// UpdateRecord updates a single record in a database.
+	// For example, this is useful in case if we want to put the record.Data to memcache.
+	// See https://github.com/dal-go/dalgo-memcache-appengine
+	// A regular DB adapter should call Update(record.Key()) inside this method.
+	UpdateRecord(ctx context.Context, record Record, updates []Update, preconditions ...Precondition) error
 }
 
 // MultiUpdater defines a function to update multiples records in database by keys
