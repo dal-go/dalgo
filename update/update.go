1cfa7ea4f3a39fcc7623bf5cf362ba8a1f70e3b3
Alexander Trakhimenok <alex@trakhimenok.com>
2025-05-05 20:04:12 +0100

    fix: update.ByFieldName can not have '.' in field name

--- update/update.go
+++ update/update.go
@@ -33,18 +33,41 @@ type Update interface {
 }
 
 func ByFieldName(fieldName string, value any) Update {
+	if fieldName == "" {
+		panic("fieldName cannot be empty")
+	}
+	if strings.Contains(fieldName, ".") {
+		v := update{fieldPath: strings.Split(fieldName, "."), value: value}
+		if err := v.Validate(); err != nil {
+			panic(err)
+		}
+		return v
+	}
 	return update{fieldName: fieldName, value: value}
 }
 
 func ByFieldPath(fieldPath FieldPath, value any) Update {
-	return update{fieldPath: fieldPath, value: value}
+	if len(fieldPath) == 0 {
+		panic("fieldPath cannot be empty")
+	}
+	v := update{fieldPath: fieldPath, value: value}
+	if err := v.Validate(); err != nil {
+		panic(err)
+	}
+	return v
 }
 
 func DeleteByFieldName(fieldName string) Update {
+	if fieldName == "" {
+		panic("fieldName cannot be empty")
+	}
 	return update{fieldName: fieldName, value: DeleteField}
 }
 
 func DeleteByFieldPath(path ...string) Update {
+	if len(path) == 0 {
+		panic("fieldPath cannot be empty")
+	}
 	return update{fieldPath: path, value: DeleteField}
 }
 
@@ -72,8 +95,16 @@ func (v update) Validate() error {
 	if strings.TrimSpace(v.fieldName) == "" && len(v.fieldPath) == 0 {
 		return errors.New("either fieldName or fieldPath must be provided")
 	}
+	if strings.Contains(v.fieldName, ".") {
+		return fmt.Errorf("fieldName contains '.' character: %q", v.fieldName)
+	}
 	if v.fieldName != "" && len(v.fieldPath) > 0 {
 		return fmt.Errorf("both FieldVal and fieldPath are provided: %v, %+v", v.fieldName, v.fieldPath)
+	}
+	for i, fp := range v.fieldPath {
+		if strings.TrimSpace(fp) == "" {
+			return fmt.Errorf("empty field path component at index %d", i)
+		}
 	}
 	return nil
 }
