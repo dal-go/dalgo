85ac7416dfa7fb5a42846d6f7cf96ebef6d4fd8e
Alexander Trakhimenok <alex@trakhimenok.com>
2025-05-26 22:23:38 +0100

    feat: Exists()

--- dal/tx_getter.go
+++ dal/tx_getter.go
@@ -2,16 +2,19 @@ package dal
 
 import "context"
 
-// Getter defines method to get a single record by key
+// Getter defines a method to get a single record by key or check its existence
 type Getter interface {
 
-	// Get gets a single record from database by key
+	// Get gets a single record from a database by key
 	Get(ctx context.Context, record Record) error
+
+	// Exists returns true if a record with the given key exists
+	Exists(ctx context.Context, key *Key) (bool, error)
 }
 
-// MultiGetter defines method to get multiples records from database by keys
+// MultiGetter defines method to get multiple records from a database by keys
 type MultiGetter interface {
 
-	// GetMulti gets multiples records from database by keys
+	// GetMulti gets multiple records from a database by keys
 	GetMulti(ctx context.Context, records []Record) error
 }
